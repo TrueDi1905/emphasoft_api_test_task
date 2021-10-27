95b4df78ad31d9035abf6d60d60d826e75fae8b7
TrueDi1905 <75692134+TrueDi1905@users.noreply.github.com>
2021-10-27 11:03:31 +0300

    'v1'

--- emphasoft_api/settings.py
+++ emphasoft_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$v9s=@=!vcolj3apu%y*sy@tytiymri6y55!3$wrmol6&v8rfw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
