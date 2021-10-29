8f46aa074d420bb0d3fd3cdd6cadb4f7a619c819
TrueDi1905 <75692134+TrueDi1905@users.noreply.github.com>
2021-10-29 17:09:44 +0300

    'v1'

--- users/exception.py
+++ users/exception.py
@@ -4,7 +4,10 @@ from rest_framework.response import Response
 
 def custom_exception_handler(exc, context):
     response = exception_handler(exc, context)
-
+    if response is None:
+        return response
     if response.status_code == 403:
         return Response({'ошибка': 'Нельзя редактировать чужой аккаунт!'})
+    if response.status_code == 401:
+        return Response({'ошибка': 'Зарегистрируйтесь пожалуйста!'})
     return response
